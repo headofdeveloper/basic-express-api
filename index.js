48d3df52b6b6ba5996fc1c44a218b05b9d73705c
Mustafa Şahin <mtafasahin@hotmail.com>
2022-01-03 11:22:27 +0300

    update index file

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var http = require('http');
+const express = require('express');
 var fs = require('fs');
+const app = express();
+const port = process.env.PORT || '3000';
 
-http.createServer(function(req, res){
+app.get('/', (req, res) => {
     fs.readFile('apple-developer-merchantid-domain-association.txt',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
         res.end();
     });
-}).listen(9090);
+});
+app.listen(port, () => {
+console.log(`listening at http://localhost:${port}`);
+});
